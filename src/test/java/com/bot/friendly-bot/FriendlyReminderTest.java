6c0cebacc073cff110d742af459f2dac29abcbdd
Vincent Tamrin <tamrin.vincent@gmail.com>
2018-08-01 18:39:16 +0700

    Update FriendlyReminderTest.java

--- src/test/java/com/bot/friendly-bot/FriendlyReminderTest.java
+++ src/test/java/com/bot/friendly-bot/FriendlyReminderTest.java
@@ -1,1 +1,16 @@
+package com.igs.bot.friendly;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FriendlyReminderTest {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
