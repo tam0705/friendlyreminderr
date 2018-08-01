e7de68c9bf989706255bf4e9ec11a750a93bb4f3
Vincent Tamrin <tamrin.vincent@gmail.com>
2018-08-01 18:46:03 +0700

    Create FriendlyReminder.java

---
+++ src/main/java/com/bot/friendly-bot/FriendlyReminder.java
@@ -0,0 +1,38 @@
+package com.friendly.bot.app;
+
+import com.linecorp.bot.client.LineMessagingClient;
+import com.linecorp.bot.model.ReplyMessage;
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.spring.boot.annotation.EventMapping;
+import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+import java.util.concurrent.ExecutionException;
+
+@SpringBootApplication
+@LineMessageHandler
+public class FriendlyReminder extends SpringBootServletInitializer {
+
+    @Autowired
+    private LineMessagingClient lineMessagingClient;
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(FriendlyReminder.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(FriendlyReminder.class, args);
+    }
+
+    @EventMapping
+    public void handleTextEvent(MessageEvent<TextMessageContent> messageEvent){
+        String message = messageEvent.getMessage().getText().toLowerCase();
+    }
+}
