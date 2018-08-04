09962c66e9eae05a97d185b8a7a0c55a34cc2ba2
tam_0705 <tamrin.vincent@gmail.com>
2018-08-04 21:22:10 +0700

    aw

--- src/main/java/com/bot/friendly-bot/FriendlyReminder.java
+++ src/main/java/com/bot/friendly-bot/FriendlyReminder.java
@@ -2,14 +2,15 @@ package com.friendly.bot;
 
 import com.linecorp.bot.client.LineMessagingClient;
 import com.linecorp.bot.model.ReplyMessage;
-import com.linecorp.bot.model.message.Message;
 import com.linecorp.bot.model.event.Event;
 import com.linecorp.bot.model.event.MessageEvent;
 import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.event.source.Source;
 import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.model.message.Message;
+import com.linecorp.bot.model.response.BotApiResponse;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
-import com.linecorp.bot.model.response.BotApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -27,6 +28,8 @@ import java.util.concurrent.ExecutionException;
 @SpringBootApplication
 @LineMessageHandler
 public class FriendlyReminder extends SpringBootServletInitializer {
+    String[] lastEditorId = new String[2];
+    String[] lastEditorName = new String[2];
 
     @Autowired
     private LineMessagingClient lineMessagingClient;
@@ -73,8 +76,67 @@ public class FriendlyReminder extends SpringBootServletInitializer {
     private void handleTextContent(String replyToken, Event event, TextMessageContent content)
             throws Exception {
         String text = content.getText();
-        /*switch (text) {
+        String userId = event.getSource().getUserId();
+        switch (text) {
+            case "/reminder tomorrow":
+            case "/reminder 0":
+            case "/rm tomorrow":
+            case "/rm 0":
+                this.showReminder(0,replyToken);
+                break;
+            case "/reminder week":
+            case "/reminder 1":
+            case "/rm week":
+            case "/rm 1":
+                this.showReminder(1,replyToken);
+                break;
+            case "/rmedit tomorrow":
+            case "/rmedit 0":
+                this.editReminder(0,replyToken,userId);
+                break;
+            case "/rmedit week":
+            case "/rmedit 1":
+                this.editReminder(1,replyToken,userId);
+                break;
+        }
+    }
 
-        }*/
+    private void showReminder(Integer param, String replyToken) {
+        String constAnswer0 = " ";
+        if (lastEditorId[param] == null) {
+            lastEditorId[param] = "U0000";
+        }
+        if (lastEditorName[param] == null) {
+            lastEditorName[param] = "unknown";
+        }
+        //A line can only has max. 112 chars, so shorterners are needed
+        String[] shorterner = new String[2];
+        shorterner[0] = lastEditorName[param];
+        shorterner[1] = lastEditorId[param].substring(0,5);
+        String constAnswer1 = "Recently edited by " + shorterner[0] + "[" + shorterner[1] + "]";
+        if (param == 0) {
+            constAnswer0 = "What to do for tomorrow is..";
+        } else if (param == 1) {
+            constAnswer0 = "This week's todo list is..";
+        }
+        this.reply(replyToken,Arrays.asList(new TextMessage(constAnswer0),new TextMessage(constAnswer1)));
+    }
+
+    private void editReminder(Integer param, String replyToken, String userId) {
+        lastEditorId[param] = "U0000";
+        lastEditorName[param] = "unknown";
+        if (userId != null) {
+                    lineMessagingClient
+                            .getProfile(userId)
+                            .whenComplete((profile, throwable) -> {
+                                if (throwable != null) {
+                                    this.replyText(replyToken, throwable.getMessage());
+                                    return;
+                                }
+                                lastEditorId[param] = userId;
+                                lastEditorName[param] = profile.getDisplayName();
+                            });
+        }
+        this.replyText(replyToken,"Successfully edited!");
     }
 }
