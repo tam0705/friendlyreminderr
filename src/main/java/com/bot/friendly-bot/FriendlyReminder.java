59646f2742f54ad3fb72460bf839ea6b66b811b1
tam_0705 <tamrin.vincent@gmail.com>
2018-08-05 20:25:22 +0700

    aw

--- src/main/java/com/bot/friendly-bot/FriendlyReminder.java
+++ src/main/java/com/bot/friendly-bot/FriendlyReminder.java
@@ -18,6 +18,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 
 import java.io.IOException;
 import java.io.OutputStream;
