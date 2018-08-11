2993962352c5e58843194424abe3145cd8e0c3d6
tam_0705 <tamrin.vincent@gmail.com>
2018-08-11 16:39:02 +0700

    Update Timestamp4.1

--- src/main/java/com/bot/friendly-bot/FriendlyReminder.java
+++ src/main/java/com/bot/friendly-bot/FriendlyReminder.java
@@ -17,17 +17,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+import javax.annotation.PostConstruct;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.UncheckedIOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.sql.*;
-import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
 
 @SpringBootApplication
