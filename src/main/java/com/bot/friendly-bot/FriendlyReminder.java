725826f84a10fc45a6ac1fab3e9e5f6477845786
tam_0705 <tamrin.vincent@gmail.com>
2018-08-22 21:31:10 +0700

    Update 2.3.18

--- src/main/java/com/bot/friendly-bot/FriendlyReminder.java
+++ src/main/java/com/bot/friendly-bot/FriendlyReminder.java
@@ -260,6 +260,7 @@ public class FriendlyReminder extends SpringBootServletInitializer {
                         isDateValid = false;
                     }
                 }
+            }
         } else {
             isDateValid = false;
         }
